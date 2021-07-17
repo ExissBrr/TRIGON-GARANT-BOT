412ff9f65426ec1099619a17564655985c5bb033
Vlad Korneev <veenrokdalv@protonmail.com>
2021-07-17 12:34:33 +0300

    v0.12.2 Добавлены кнопки отмены при поиске

--- app/handlers/private/admin/message/menu/search/request_data_for_search.py
+++ app/handlers/private/admin/message/menu/search/request_data_for_search.py
@@ -1,5 +1,6 @@
 from aiogram.types import Message
 
+from app import keyboards
 from app.data import text
 from app.loader import dp
 from app.data.text.ru.admin.button.reply import search
@@ -10,6 +11,7 @@ from app.states.private.search import SearchStates
 @dp.message_handler(reply_command=search, user_role=UserRole.ADMIN)
 async def request_data_for_search(message: Message, lang_code):
     await message.answer(
-        text=text[lang_code].admin.message.ask_for_user_data
+        text=text[lang_code].admin.message.ask_for_user_data,
+        reply_markup=keyboards.default.reply.cancel.keyboard(lang_code)
     )
     await SearchStates.wait_for_data.set()
--- app/keyboards/admin/reply/menu_message_distribution.py
+++ app/keyboards/admin/reply/menu_message_distribution.py
@@ -8,12 +8,11 @@ def make_keyboard(lang_code):
         resize_keyboard=True,
         keyboard=[
             [
-                KeyboardButton(
-                    text=text[lang_code].admin.button.reply.distribution_settings
-                ),
-                KeyboardButton(
-                    text=text[lang_code].admin.button.reply.send_messages
-                )
+                KeyboardButton(text[lang_code].default.message.cancel)
+            ],
+            [
+                KeyboardButton(text[lang_code].admin.button.reply.distribution_settings),
+                KeyboardButton(text[lang_code].admin.button.reply.send_messages)
             ]
         ]
     )
