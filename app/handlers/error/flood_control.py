c02f049d665ef50f5db969a726f2de5c17540214
Vlad Korneev <veenrokdalv@protomail.com>
2021-08-02 05:21:42 +0300

    4.1.1 Fix commit Исправлены ошибки в защите от флудильщиков

--- app/handlers/error/flood_control.py
+++ app/handlers/error/flood_control.py
@@ -1,19 +1,36 @@
 import asyncio
 import time
+from builtins import isinstance
 
 from aiogram.dispatcher.handler import CancelHandler
 from aiogram.types import Update
 from aiogram.utils.exceptions import RetryAfter
+from loguru import logger
 
 import app
-from app.loader import dp, flood_defender_time
+from app.loader import dp, flood_defender_time, flood_user_in_processing
 
 
-@dp.errors_handler(exception=RetryAfter)
-async def on_flood_defender(update: Update, exception: RetryAfter):
-    app.loader.is_flood_defender = True
-    time.sleep(exception.timeout + 5)
+@dp.errors_handler()
+async def on_flood_defender(update: Update, exception):
+    if isinstance(exception, RetryAfter):
+        logger.warning(f'Бот упал')
+        if app.loader.is_flood_defender:
+            raise CancelHandler
 
-    await asyncio.sleep(flood_defender_time)
-    app.loader.is_flood_defender = False
-    raise CancelHandler()
+        app.loader.is_flood_defender = True
+
+        updates = await dp.bot.get_updates()
+        await dp.process_updates(updates)
+
+        time.sleep(exception.timeout+4)
+
+        updates = await dp.bot.get_updates()
+
+        await dp.process_updates(updates)
+
+        await asyncio.sleep(flood_defender_time)
+
+        app.loader.is_flood_defender = False
+
+        flood_user_in_processing.clear()
