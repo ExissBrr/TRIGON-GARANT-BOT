04fd70c09a24965479ed51cba2db0b1435973d0f
Exiss <vladbelykh2002@gmail.com>
2021-08-11 17:23:28 +0300

    v1.18.0 рассылка в меню админа

--- app/handlers/private/admin/callback_query/menu/__init__.py
+++ app/handlers/private/admin/callback_query/menu/__init__.py
@@ -1,4 +1,4 @@
 from .show_controversy_deals_list import dp
 from .seller_menu import dp
-
+from .sending_messages import dp
 __all__ = ['dp']
--- app/handlers/private/admin/callback_query/menu/sending_messages/wait_messages.py
+++ app/handlers/private/admin/callback_query/menu/sending_messages/wait_messages.py
@@ -1,18 +1,19 @@
-from aiogram.types import Message, CallbackQuery
-from data import text
-from filters.private.role_user import UserRoleFilter
-from keyboards import reply
-from keyboards.callback_data.admin.admin_menu_choice import admin_menu_cd, AdminMenuChoice
-from loader import dp
-from states.admin.sending_messages import StatesSendingMessages
-from utils.database_api.table_models.user import UserRole
+from aiogram.types import CallbackQuery
+
+from app import keyboards
+from app.data import text
+from app.data.types.user_data import UserRole
+from app.filters import UserRoleFilter
+from app.keyboards.callback_data.admin import admin_menu_cd, AdminMenuChoice
+from app.loader import dp
+from app.states.private.message_distribution import MomentaryMessageSendingStates
 
 
 @dp.callback_query_handler(UserRoleFilter(UserRole.ADMIN), admin_menu_cd.filter(command=AdminMenuChoice.SEND_MESSAGES))
-async def wait_message(call: CallbackQuery):
-    await call.answer(cache_time=5)
+async def wait_for_roles(call: CallbackQuery, lang_code):
+    await MomentaryMessageSendingStates.wait_for_roles.set()
+    roles = UserRole.ROLES
     await call.message.answer(
-        text='Введите сообщение, которое нужно разослать',
-        reply_markup=reply.default.cancel.keyboard
+        text=text[lang_code].default.message.send_distribution_roles,
+        reply_markup=keyboards.default.reply.skip_and_roles.keyboard(roles, lang_code),
     )
-    await StatesSendingMessages.wait_messages.set()
